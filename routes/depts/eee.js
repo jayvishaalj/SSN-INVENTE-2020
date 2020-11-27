1b6f5ea8c252908ce93012bb8dd9488ec3834bbe
jayvishaalj <jayvishaalj.01@gmail.com>
2020-11-27 15:51:55 +0530

    Half of the dept pages addded

--- routes/depts/eee.js
+++ routes/depts/eee.js
@@ -2,22 +2,22 @@ var express = require("express");
 var router = express.Router();
 const logger = require("../../config/logger")(module);
 
-/* GET ECE Dept Page */
+/* GET CSE Dept Page */
 router.get("/", function (req, res, next) {
   const messages = req.flash();
   if (req.session.isLogged) {
     if (req.session.data) {
-      logger.log("info", `User Entered ECE Page ${req.session.data.name}`);
-      return res.render("ece", {
+      logger.log("info", `User Entered EEE Page ${req.session.data.name}`);
+      return res.render("eee", {
         messages,
         username: req.session.data.name,
         isLogged: req.session.isLogged,
       });
     } else {
-      return res.render("ece", { messages });
+      return res.render("eee", { messages });
     }
   } else {
-    return res.render("ece", { messages });
+    return res.render("eee", { messages });
   }
 });
 
--- routes/depts/router.js
+++ routes/depts/router.js
@@ -1,7 +1,15 @@
 var express = require("express");
 var router = express.Router();
+var cseDeptRouter = require("./cse");
+var itDeptRouter = require("./it");
 var eceDeptRouter = require("./ece");
+var civilDeptRouter = require("./civil");
+var eeeDeptRouter = require("./eee");
 
+router.use("/cse", cseDeptRouter);
+router.use("/it", itDeptRouter);
 router.use("/ece", eceDeptRouter);
+router.use("/civil", civilDeptRouter);
+router.use("/eee", eeeDeptRouter);
 
 module.exports = router;
